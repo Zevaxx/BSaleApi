de740d514f4a06ea953a1c5157162576f78b20ff
Sebastián Díaz <sebastian88@gmail.com>
2021-03-14 21:43:32 -0300

    BSale API Last Api

--- server/db/index.js
+++ server/db/index.js
@@ -1,38 +1,66 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-user: 'xx',
-database: 'xx',
-password: 'xx',
-host: 'xx'
+  user: 'bsale_test',
+  database: 'bsale_test',
+  password: 'bsale_test',
+  host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com'
 
 });
 
-let bsaledb = {
+const bsaledb = {};
 
-};
-
-bsaledb.all = () => {
-    return new Promise((resolve, reject) =>{
-            pool.query('SELECT * FROM product;', (err,results)=>{
-                if(err){
-                return reject(err);
-                }
-                return resolve(results);
-            });
+bsaledb.productos = () => {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM product;', (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(results);
     });
+  });
 };
 
 bsaledb.search = (buscar) => {
-    return new Promise((resolve, reject) =>{
-             let like = '%' + buscar + '%';
-             pool.query('Select * FROM product where name like ? ;', like , (err,results)=>{
-                if(err){
-                return reject(err);
-                }
-                return resolve(results);
-            });
+  return new Promise((resolve, reject) => {
+    const like = `%${buscar}%`;
+    pool.query('Select * FROM product where name like ? ;', like, (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(results);
     });
+  });
+};
+
+bsaledb.categories = () => {
+  return new Promise((resolve, reject) => {
+    pool.query(`
+      select DISTINCT category.name , category.id 
+      from product
+      inner join category
+      on product.category = category.id;`, (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
+};
+
+bsaledb.selectCategory = (filtro) => {
+  return new Promise((resolve, reject) => {
+    const category = `${filtro}`;
+    pool.query(`
+    select *
+    from product
+    where category = ? ;`, category, (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(results);
+    });
+  });
 };
 
 module.exports = bsaledb;
--- server/server.js
+++ server/server.js
@@ -5,23 +5,20 @@ const apiRouter = require('./routes');
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
 
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  // authorized headers for preflight requests
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 
-app.options('*', (req, res) => {
-        // allowed XHR methods  
-     res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-     res.send();
-    });
+  app.options('*', (req, res) => {
+    // allowed XHR methods
+    res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+    res.send();
+  });
 });
-app.use('/api/productos', apiRouter)
+app.use('/api', apiRouter);
 
 app.listen(process.env.PORT || '3000', () => {
-
-    console.log(`Server is running on port: ${process.env.PORT || '3000'}`)
-
+  console.log(`Server is running on port: ${process.env.PORT || '3000'}`);
 });
